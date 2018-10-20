825a1508ea043100391073dfa435d06ab11c0428
buobao <lebron_king_james@163.com>
2018-10-20 11:39:55 +0800

    init commit

---
+++ app/src/main/java/com/king/turman/talkme/TalkMeApplication.java
@@ -0,0 +1,15 @@
+package com.king.turman.talkme;
+
+import android.app.Application;
+
+/**
+ * Created by diaoqf on 2018/10/19.
+ */
+
+public class TalkMeApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
