55627f967344c5e578e03c30533c82a82a823bbb
buobao <lebron_king_james@163.com>
2018-10-22 18:02:03 +0800

    add retrofit2

---
+++ app/src/main/java/com/king/turman/talkme/network/NetWorkManager.java
@@ -0,0 +1,55 @@
+package com.king.turman.talkme.network;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by diaoqf on 2018/10/22.
+ */
+public class NetWorkManager {
+
+    private static NetWorkManager mInstance;
+    private static Retrofit retrofit;
+    private static volatile Request request = null;
+
+    public static NetWorkManager getInstance() {
+        if (mInstance == null) {
+            synchronized (NetWorkManager.class) {
+                if (mInstance == null) {
+                    mInstance = new NetWorkManager();
+                }
+            }
+        }
+        return mInstance;
+    }
+
+    /**
+     * 初始化必要对象和参数
+     */
+    public void init() {
+        // 初始化okhttp
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+
+        // 初始化Retrofit
+        retrofit = new Retrofit.Builder()
+                .client(client)
+                .baseUrl("")
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public static Request getRequest() {
+        if (request == null) {
+            synchronized (Request.class) {
+                request = retrofit.create(Request.class);
+            }
+        }
+        return request;
+    }
+
+}
